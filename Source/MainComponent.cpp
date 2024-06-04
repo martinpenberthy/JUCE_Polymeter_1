b97b0795565cabb84432f3d7a6299d6fd85ef08f
Martin <martinpenberthy1@gmail.com>
2024-06-04 14:19:14 -0700

    First commit

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,136 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+
+    // Some platforms require permissions to open input channels so request that here
+    if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
+        && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
+    {
+        juce::RuntimePermissions::request (juce::RuntimePermissions::recordAudio,
+                                           [&] (bool granted) { setAudioChannels (granted ? 2 : 0, 2); });
+    }
+    else
+    {
+        // Specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
+    
+    startTimer(30);
+    
+    addAndMakeVisible(&sliderTime);
+    sliderTime.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    sliderTime.setRange(10.0f, 1000.0f);
+    sliderTime.addListener(this);
+    
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+}
+void MainComponent::sliderValueChanged (juce::Slider* slider)
+{
+    if(slider == &sliderTime)
+    {
+        stopTimer();
+        startTimer(sliderTime.getValue());
+    }
+}
+
+void MainComponent::timerCallback()
+{
+    if(getTimerInterval() == 30)
+    {
+        isBeep = !isBeep;
+        
+        DBG("isBeep flipped");
+    }
+    return;
+}
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    
+    currentSampleRate = sampleRate;
+    
+    auto cyclesPerSample = currentFrequency / currentSampleRate;         // [2]
+    angleDelta = cyclesPerSample * 2.0 * juce::MathConstants<double>::pi;          // [3]
+    
+    isBeep = true;
+}
+
+void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    // Your audio-processing code goes here!
+
+    // For more details, see the help for AudioProcessor::getNextAudioBlock()
+
+    // Right now we are not producing any data, in which case we need to clear the buffer
+    // (to prevent the output of random noise)
+    bufferToFill.clearActiveBufferRegion();
+    
+    auto level = 0.0f;
+    
+    if(isBeep)
+    {
+        level = 0.125f;
+        auto* leftBuffer  = bufferToFill.buffer->getWritePointer (0, bufferToFill.startSample);
+        auto* rightBuffer = bufferToFill.buffer->getWritePointer (1, bufferToFill.startSample);
+        
+        for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
+        {
+            auto currentSample = (float) std::sin (currentAngle);
+            currentAngle += angleDelta;
+            leftBuffer[sample]  = currentSample * level;
+            rightBuffer[sample] = currentSample * level;
+        }
+        DBG("Beep on");
+    }
+    else
+    {
+        level = 0.0f;
+        auto* leftBuffer  = bufferToFill.buffer->getWritePointer (0, bufferToFill.startSample);
+        auto* rightBuffer = bufferToFill.buffer->getWritePointer (1, bufferToFill.startSample);
+        
+        for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
+        {
+            auto currentSample = (float) std::sin (currentAngle);
+            currentAngle += angleDelta;
+            leftBuffer[sample]  = currentSample * level;
+            rightBuffer[sample] = currentSample * level;
+        }
+        DBG("Beep off");
+    }
+}
+
+void MainComponent::releaseResources()
+{
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    // You can add your drawing code here!
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+    
+    sliderTime.setBounds(100, 100, 20, 300);
+}
---
+++ Source/MainComponent.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent  : public juce::AudioAppComponent, public juce::Timer, public juce::Slider::Listener
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent() override;
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    //==============================================================================
+    void paint (juce::Graphics& g) override;
+    void resized() override;
+
+    void timerCallback() override;
+    void sliderValueChanged (juce::Slider* slider) override;
+
+private:
+    //==============================================================================
+    // Your private member variables go here...
+    double currentSampleRate = 0.0, currentAngle = 0.0, angleDelta = 0.0;
+    double currentFrequency = 500.0, targetFrequency = 500.0; // [5]
+    
+    bool isBeep;
+    
+    juce::Slider sliderTime;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
